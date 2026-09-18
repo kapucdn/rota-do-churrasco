f1a61cd1a189ed292764c8122d8d88d6abae2a44
Alan Rodrigues Fernandes da Silva <alandesktop@gmail.com>
2026-09-18 15:44:06 -0300

    adicionei imagens

--- api/admin.js
+++ api/admin.js
@@ -15,10 +15,13 @@ export default async function handler(req, res) {
   }
 
   const { senha, acao, dados } = req.body || {};
-
-  if (!process.env.ADMIN_PASSWORD || senha !== process.env.ADMIN_PASSWORD) {
+  if (senha !== "Senha@rota") {
     return res.status(401).json({ erro: "Senha incorreta." });
   }
+
+  // if (!process.env.ADMIN_PASSWORD || senha !== process.env.ADMIN_PASSWORD) {
+  //   return res.status(401).json({ erro: "Senha incorreta." });
+  // }
 
   const SUPABASE_URL = process.env.SUPABASE_URL;
   const SERVICE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
